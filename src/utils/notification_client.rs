618f151841da5c45f78c68b62506adb0e918ca25
Ravi Sawlani <ravisawlani04@gmail.com>
2026-03-26 14:06:26 +0530

    fix metadata endpoint (#4)
    * fix metadata endpoint
    
    * add sentry messages incase notification fails

--- src/utils/notification_client.rs
+++ src/utils/notification_client.rs
@@ -4,7 +4,7 @@ use candid::Principal;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
-const METADATA_SERVER_URL: &str = "https://yral-metadata.fly.dev";
+const METADATA_SERVER_URL: &str = "https://metadata.yral.com";
 
 #[derive(Clone, Debug)]
 pub struct NotificationClient {
@@ -37,12 +37,28 @@ impl NotificationClient {
         match res {
             Ok(response) => {
                 if response.status().is_success() {
+                    log::info!(
+                        "Notification sent successfully to user {}",
+                        user_principal.to_text()
+                    );
                 } else if let Ok(body) = response.text().await {
-                    eprintln!("Response body: {}", body);
+                    let msg = format!(
+                        "Failed to send notification to user {}: {}",
+                        user_principal.to_text(),
+                        body
+                    );
+                    log::error!("{}", msg);
+                    sentry::capture_message(&msg, sentry::Level::Error);
                 }
             }
             Err(req_err) => {
-                eprintln!("Error sending notification request for : {}", req_err);
+                let msg = format!(
+                    "Error sending notification request to user {}: {}",
+                    user_principal.to_text(),
+                    req_err
+                );
+                log::error!("{}", msg);
+                sentry::capture_message(&msg, sentry::Level::Error);
             }
         }
     }
