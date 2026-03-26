09367cf0275edb5cd8f8815ee312e4bf527e85da
Ravi Sawlani <ravisawlani04@gmail.com>
2026-03-26 14:00:21 +0530

    add sentry messages incase notification fails

--- src/utils/notification_client.rs
+++ src/utils/notification_client.rs
@@ -37,12 +37,28 @@ impl NotificationClient {
         match res {
             Ok(response) => {
                 if response.status().is_success() {
+                    log::info!(
+                        "Notification sent successfully to user {}",
+                        user_principal.to_text()
+                    );
                 } else if let Ok(body) = response.text().await {
-                    eprintln!("Response body: {}", body);
+                    let msg = format!(
+                        "Failed to send notification to user {}: {}",
+                        user_principal.to_text(),
+                        body
+                    );
+                    log::error!("{}", msg);
+                    sentry::capture_message(&msg, sentry::Level::Error);
                 }
             }
             Err(req_err) => {
-                eprintln!("Error sending notification request for : {}", req_err);
+                let msg = format!(
+                    "Error sending notification request to user {}: {}",
+                    user_principal.to_text(),
+                    req_err
+                );
+                log::error!("{}", msg);
+                sentry::capture_message(&msg, sentry::Level::Error);
             }
         }
     }
